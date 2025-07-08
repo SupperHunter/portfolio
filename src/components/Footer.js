66582f0382d0b7514a17bf8e8e447bf1ec9c9327
ChienTQ2003 <anhchienbs.1@gmail.com>
2025-07-08 23:26:58 +0700

    commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,9 +6,6 @@ class Footer extends Component {
       var networks = this.props.sharedBasicInfo.social.map(function (network) {
         return (
           <span key={network.name} className="m-4">
-            <a href={network.url} target="_blank" rel="noopener noreferrer">
-              <i className={network.class}></i>
-            </a>
           </span>
         );
       });
